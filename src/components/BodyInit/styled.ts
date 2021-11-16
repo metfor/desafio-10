8ab3fcfcb2a75fa84496e1eeffb449469d47ac8e
metfor <mateus_frade@hotmail.com>
2021-11-16 17:33:08 -0300

    feat: start body

---
+++ src/components/BodyInit/styled.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+export const BodyContainer = styled.div`
+    width: 80%;
+    margin-left: 10%;
+    margin-right: 10%;
+    display: flex;
+    align-items: left;
+`
