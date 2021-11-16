3fa612e69352f9ad7db3b2fb8ffb74f48ed0cdd5
metfor <mateus_frade@hotmail.com>
2021-11-16 17:45:26 -0300

    feat: finalizando component BodyInit

---
+++ src/components/BodyInit/index.tsx
@@ -0,0 +1,19 @@
+import { BodyContainer, Desc, Title } from './styled';
+
+export function BodyInit() {
+  return (
+    <BodyContainer>
+      <Title>
+        Mundo nerd?
+        <br />
+        Naped!
+        <br/>
+        <Desc>
+        O Naped pode ser sua fonte de informações sobre
+        <br/>
+        o mundo nerd e outros assuntos relacionados.
+        </Desc>
+      </Title>
+    </BodyContainer>
+  );
+}
--- src/components/BodyInit/styled.ts
+++ src/components/BodyInit/styled.ts
@@ -1,8 +1,23 @@
 import styled from 'styled-components'
 export const BodyContainer = styled.div`
+    margin-top:100px;
     width: 80%;
     margin-left: 10%;
     margin-right: 10%;
-    display: flex;
+    display: grid;
     align-items: left;
+`
+export const Title = styled.h1`
+    font-family: Lexend Deca;
+    font-size:48px;
+    line-height:50px;
+    font-weight:400;
+    color:#fbfbfb;
+`
+export const Desc = styled.text`
+    font-family: Inter;
+    font-size:20px;
+    line-height:30px;
+    font-weight:500;
+    color:#c4c4c4;
 `
