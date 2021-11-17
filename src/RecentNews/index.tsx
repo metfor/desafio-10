cc569e74a43bcfa81ba1c670cd484aac79ee53a8
metfor <mateus_frade@hotmail.com>
2021-11-17 11:13:53 -0300

    feat: componente RecentNews finalizado

---
+++ src/RecentNews/index.tsx
@@ -0,0 +1,118 @@
+import { ButtonLorem, TextSideBar } from '../Body/styled';
+import {
+  BarrPurple,
+  BoxImg,
+  BoxNew,
+  BoxRecentNews,
+  ImgNext,
+  RecentNewsContainer,
+  Shadow,
+  Title,
+} from './styled';
+
+export function RecentNews() {
+  return (
+    <RecentNewsContainer>
+      <Title>Notícias mais relevantes</Title>
+      <BarrPurple />
+      <BoxRecentNews>
+        <BoxNew>
+          <BoxImg>
+            <Shadow />
+            <ButtonLorem>Lorem</ButtonLorem>
+            <TextSideBar>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+              eros tellus, malesuada et velit in, blandit molestie dolor.
+            </TextSideBar>
+            <ImgNext
+              src="/../public/newRe1.svg"
+              width="338"
+              height="250"
+              alt="recent news"
+            />
+          </BoxImg>
+        </BoxNew>
+        <BoxNew>
+          <BoxImg>
+            <Shadow />
+            <ButtonLorem>Lorem</ButtonLorem>
+            <TextSideBar>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+              eros tellus, malesuada et velit in, blandit molestie dolor.
+            </TextSideBar>
+            <ImgNext
+              src="/../public/newRe2.svg"
+              width="338"
+              height="250"
+              alt="recent news"
+            />
+          </BoxImg>
+        </BoxNew>
+        <BoxNew>
+          <BoxImg>
+            <Shadow />
+            <ButtonLorem>Lorem</ButtonLorem>
+            <TextSideBar>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+              eros tellus, malesuada et velit in, blandit molestie dolor.
+            </TextSideBar>
+            <ImgNext
+              src="/../public/newRe3.svg"
+              width="338"
+              height="250"
+              alt="recent news"
+            />
+          </BoxImg>
+        </BoxNew>
+        <BoxNew>
+          <BoxImg>
+            <Shadow />
+            <ButtonLorem>Lorem</ButtonLorem>
+            <TextSideBar>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+              eros tellus, malesuada et velit in, blandit molestie dolor.
+            </TextSideBar>
+            <ImgNext
+              src="/../public/newRe4.svg"
+              width="338"
+              height="250"
+              alt="recent news"
+            />
+          </BoxImg>
+        </BoxNew>
+        <BoxNew>
+          <BoxImg>
+            <Shadow />
+            <ButtonLorem>Lorem</ButtonLorem>
+            <TextSideBar>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+              eros tellus, malesuada et velit in, blandit molestie dolor.
+            </TextSideBar>
+            <ImgNext
+              src="/../public/newRe5.svg"
+              width="338"
+              height="250"
+              alt="recent news"
+            />
+          </BoxImg>
+        </BoxNew>
+        <BoxNew>
+          <BoxImg>
+            <Shadow />
+            <ButtonLorem>Lorem</ButtonLorem>
+            <TextSideBar>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+              eros tellus, malesuada et velit in, blandit molestie dolor.
+            </TextSideBar>
+            <ImgNext
+              src="/../public/newRe6.svg"
+              width="338"
+              height="250"
+              alt="recent news"
+            />
+          </BoxImg>
+        </BoxNew>
+      </BoxRecentNews>
+    </RecentNewsContainer>
+  );
+}
